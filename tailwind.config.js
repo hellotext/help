18e7dede022b227745a4f2fe168b64a1c3b6b29d
Martin Caetano <martin.caetano@icloud.com>
2024-05-26 18:42:32 -0300

    Adds new improved UI.

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,6 +28,7 @@ module.exports = {
         transparent: "transparent",
         banana: {
           DEFAULT: "var(--banana)",
+          10: "var(--banana-10)",
           20: "var(--banana-20)",
           40: "var(--banana-40)",
           60: "var(--banana-60)",
@@ -49,6 +50,7 @@ module.exports = {
         },
         lavender: {
           DEFAULT: "var(--lavender)",
+          60: "var(--lavender-60)",
           light: "var(--lavender-light)",
         },
         night: {
@@ -63,21 +65,73 @@ module.exports = {
         seafoam: {
           DEFAULT: "var(--seafoam)",
           20: "var(--seafoam-20)",
+          30: "var(--seafoam-30)",
           40: "var(--seafoam-40)",
           60: "var(--seafoam-60)",
           80: "var(--seafoam-80)",
         },
         tiger: {
           DEFAULT: "var(--tiger)",
+          10: "var(--tiger-10)",
           20: "var(--tiger-20)",
           40: "var(--tiger-40)",
           60: "var(--tiger-60)",
           80: "var(--tiger-80)",
         },
       },
+      fontSize: {
+        "4xl": [
+          "2.125rem", // 34px
+          {
+            lineHeight: "2.625rem", // 42px
+          },
+        ],
+        "5xl": [
+          "3rem", // 48px
+          {
+            lineHeight: "3.25rem", // 52px
+          },
+        ],
+        "6xl": [
+          "3.5rem", // 56px
+          {
+            lineHeight: "4rem",
+          },
+        ],
+        "7xl": [
+          "5rem", // 80px
+          {
+            lineHeight: "5.125rem", // 82px
+          },
+        ],
+      },
     },
   },
-  safelist: ["bg-blush"],
+  safelist: [
+    "bg-blush",
+    "bg-tiger-10",
+    "bg-banana-10",
+    "bg-seafoam-30",
+    "bg-lavender-60",
+    "hover:shadow-blush",
+    "hover:shadow-tiger-10",
+    "hover:shadow-banana-10",
+    "hover:shadow-seafoam-30",
+    "hover:shadow-lavender-60",
+    "shadow-blush",
+    "shadow-tiger-10",
+    "shadow-banana-10",
+    "shadow-seafoam-30",
+    "shadow-lavender-60",
+    "border-lavender",
+    "border-tiger-40",
+    "border-banana-60",
+    "border-seafoam",
+    "hover:border-lavender",
+    "hover:border-tiger-40",
+    "hover:border-banana-60",
+    "hover:border-seafoam",
+  ],
   variants: {},
   plugins: [require("@tailwindcss/typography")],
 };
